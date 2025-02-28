b6ac1cc6394289111c7e031d6ab9011400a6defc
Arthur Grillo <arthur.grillo@usp.br>
2025-02-27 23:06:18 -0300

    vinumc/tests: Test output flag
    We don't have a test that checks the output flag working, create
    a simple one.

---
+++ subprojects/vinumc/tests/output_flag_test.c
@@ -0,0 +1,25 @@
+#include <vunit.h>
+#include <stdlib.h>
+
+void test_output_flag(struct vunit_test_ctx *ctx) {
+	char *out = NULL;
+
+	vunit_run_vinumc_ok(ctx,
+		"[a: Hello World!]\n"
+		"[a]\n",
+		&out,
+		"--output",
+		"out.txt",
+		NULL
+	);
+
+	char *out_file_str = vunit_file_to_str(ctx, "out.txt");
+	VUNIT_ASSERT_STREQ(ctx, out, "");
+
+	VUNIT_ASSERT_STREQ(ctx, out_file_str, "Hello World!\n");
+}
+
+VUNIT_TEST_SUITE("suite",
+	{"Test output flag", test_output_flag},
+	{},
+)
