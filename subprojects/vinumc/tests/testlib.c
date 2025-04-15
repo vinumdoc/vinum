537270b5f6412c9208b7b921d98c08b528fa44b4
Arthur Pin <arthur_pin@usp.br>
2025-04-15 16:26:53 -0300

    subprojects/vinumc: Add test library This library is used for the unit tests.

---
+++ subprojects/vinumc/tests/testlib.c
@@ -0,0 +1,10 @@
+#include "extern_library.h"
+
+struct return_value return_arg(char *arg) {
+	return (struct return_value){ .ptr = arg };
+}
+
+struct extern_function *expose_library() {
+	static struct extern_function lib[] = { { "return_arg", return_arg }, {} };
+	return lib;
+}
