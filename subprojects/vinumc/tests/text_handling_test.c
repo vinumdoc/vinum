6a48592759467079e42edaaa70203c7e6d5e7beb
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2025-06-29 10:45:26 -0300

    vinumc/dry_flex: implement character escaping

--- subprojects/vinumc/tests/text_handling_test.c
+++ subprojects/vinumc/tests/text_handling_test.c
@@ -11,8 +11,20 @@ void test_blank_reduction(struct vunit_test_ctx *ctx) {
 	VUNIT_ASSERT_STREQ(ctx, out, "Content with spaces!");
 }
 
+void test_character_escaping(struct vunit_test_ctx *ctx) {
+	char *out = NULL;
+
+	vunit_run_vinumc_ok(ctx,
+			    "[a: \\[ hello \\] \\{# world #}!]\n"
+			    "[a]\n",
+			    &out, NULL);
+
+	VUNIT_ASSERT_STREQ(ctx, out, "[ hello ] {# world #}!");
+}
+
 struct vunit_test tests[] = {
 	{ .name = "Test blank reduction", .test_func = test_blank_reduction },
+	{ .name = "Test character escaping", .test_func = test_character_escaping },
 	{},
 };
 
