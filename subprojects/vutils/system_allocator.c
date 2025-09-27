02dcd9f3c5bb7fdbfdb13568a89a8a70981065f1
Arthur Grillo <arthur.grillo@usp.br>
2025-09-27 20:59:45 -0300

    vutils/allocator: Implement the interface for the system allocator
    Let's start to use the new allocator interface.

---
+++ subprojects/vutils/system_allocator.c
@@ -0,0 +1,42 @@
+#include "allocator.h"
+#include <stdlib.h>
+
+static void *sys_malloc(void *_, size_t bytes, size_t times) {
+	(void)_;
+	if (bytes * times == 0)
+		return NULL;
+	return malloc(bytes * times);
+}
+
+static void *sys_calloc(void *_, size_t bytes, size_t times) {
+	(void)_;
+	if (bytes * times == 0)
+		return NULL;
+	return calloc(times, bytes);
+}
+
+static void *sys_realloc(void *_, void *ptr, size_t bytes, size_t times) {
+	(void)_;
+	return realloc(ptr, bytes * times);
+}
+
+static void sys_free(void *_, void *ptr) {
+	(void)_;
+	free(ptr);
+}
+
+static const struct vut_allocator_funcs system_alloc_funcs = {
+	.malloc = sys_malloc,
+	.calloc = sys_calloc,
+	.realloc = sys_realloc,
+	.free = sys_free,
+};
+
+struct vut_allocator sys_allocator = {
+	.base_allocator = NULL,
+	.funcs = system_alloc_funcs,
+};
+
+struct vut_allocator *vut_get_system_allocator() {
+	return &sys_allocator;
+}
