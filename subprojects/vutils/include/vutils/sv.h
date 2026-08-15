9a0fe41a2d559f3e57543ccc9dac2b7ce9797d77
Arthur Grillo <arthurgrillo@riseup.net>
2026-08-15 14:00:31 -0300

    vutils: Implement vut_sv_splitc

--- subprojects/vutils/include/vutils/sv.h
+++ subprojects/vutils/include/vutils/sv.h
@@ -24,4 +24,6 @@ char *vut_sv_to_cstr(const struct vut_sv sv, struct vut_allocator alloc);
 bool vut_sv_eq(struct vut_sv a, struct vut_sv b);
 bool vut_sv_is_empty(struct vut_sv sv);
 
+struct vut_sv vut_sv_splitc(struct vut_sv *sv, char c);
+
 #endif // __VUT_SV_H__
